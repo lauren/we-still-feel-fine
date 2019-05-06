cc7f585a6f2f3e9209aa94dbfaed2b0a2b4d54de
lauren <lauren.sperber@gmail.com>
2019-05-06 17:17:47 -0400

    upgrades jade to pug

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (env === "development") {
 
 // app configuration
 app.use(express.static(__dirname + "/public"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 app.set("view options", {layout: false});
 app.set("views", __dirname + "/views");
 app.use(morgan("dev"));
